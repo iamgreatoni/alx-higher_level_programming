7c64a28c3e5769f08974ea42381eb2900b1cf81f
Oni John <onijohnoluwaseun@gmail.com>
2022-09-06 06:17:04 +0100

    Task Completed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-if (num) {
-  console.log('My number: ' + num);
-} else {
-  console.log('Not a number');
-}
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(firstArg + ' is ' + secondArg);
