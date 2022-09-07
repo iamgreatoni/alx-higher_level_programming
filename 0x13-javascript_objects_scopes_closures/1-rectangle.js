3a46035e62f33689857e6aa0e9c68c73d39741be
Oni John <onijohnoluwaseun@gmail.com>
2022-09-07 03:56:16 +0100

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+/*
+    defining a rectangle class with the propeties
+    hieght and width
+*/
 class Rectangle {
     constructor (w, h) {
       this.width = w;
       this.height = h;
-    };
-};
+    }
+}
+  
 module.exports = Rectangle;
