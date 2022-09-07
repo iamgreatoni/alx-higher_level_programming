b4e984ec56884600a0e64f7a0d5320888272712b
Oni John <onijohnoluwaseun@gmail.com>
2022-09-07 05:59:42 +0100

    Task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const arr = require('./100-data');
+const newList = arr.list.map((elem, idx) => elem * idx);
+console.log(arr.list);
+console.log(newList);
