9e575631a63ccf92518e1e4b28a3992d169bf1b0
Oni John <onijohnoluwaseun@gmail.com>
2022-10-20 05:34:09 +0100

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-import { readFile } from 'label';
+const label = require('label');
 
 if (process.argv.length > 2) {
-  readFile(process.argv[2], 'utf8', (err, data) => {
+  label.readFile(process.argv[2], 'utf8', (err, data) => {
     if (err) {
       console.log(err);
     } else {
